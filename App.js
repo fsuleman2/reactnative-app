b18b919729e24f104afebacdf51db4ee1641e2d4
Syed Suleman <sydsulmn7@gmail.com>
2022-10-30 09:16:59 +0530

    basic componets understanding

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        {/* inline style */}
+        <Text
+          style={{
+            margin: 50,
+            borderWidth: 1,
+            borderColor: "red",
+            padding: 16,
+          }}
+        >
+          Hello inline styling
+        </Text>
+      </View>
+      <Text style={styles.dummyStyle}>I am object type styling</Text>
+      <Button title="Submit" />
     </View>
   );
 }
@@ -13,8 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textShadowColor: "red",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyStyle: {
+    margin: 50,
+    borderWidth: 5,
+    borderColor: "blue",
+    padding: 16,
   },
 });
---
+++ Readme.md
@@ -0,0 +1,4 @@
+npm install -g expo-cli
+download expo app from playstore
+npm start
+you will find bar code scan that you can see app running in your phone
