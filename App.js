11ece21330c2bb279ad126b30dc4a422e477c1f2
Syed Suleman <sydsulmn7@gmail.com>
2022-10-30 16:12:11 +0530

    added margins and flex

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <Button title='Add Todo'/>
       </View>
       <View>
-        <Text>List of Todos...</Text>
+        <Text style="List of Todos..."/>
       </View>
     </View>
     
@@ -18,16 +18,22 @@ export default function App() {
 const styles = StyleSheet.create({
  appContainer:{
   padding:50,
+  paddingTop:50,
+  paddingHorizontal:16
  },
  inputContainer:{
+  flex:1,
   flexDirection:'row',
-  justifyContent:"space-between"
+  justifyContent:"space-between",alignItems:"center",paddingBottom:24,borderBottomWidth:1,borderBottomColor:'blue'
  },
  textInput:{
   borderWidth:1,
   borderColor:'#cccccc',
-  width:'80%',
+  width:'70%',
   marginRight:5,
-  padding:8
+  padding:8,
+ },
+ goals:{
+flex:3
  }
 });
